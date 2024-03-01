9f497fbd9d0a5d3c51dbdd9f4c8091fed6a8e6f1
Diego Monterroso <149099355+Dmonterro@users.noreply.github.com>
2024-03-01 11:49:55 -0600

    Add files via upload

---
+++ Ejercicio2.py
@@ -0,0 +1,9 @@
+def inverso_palabra(cadena):
+    
+    return cadena[::-1]
+
+
+cadena_original = "P,Y,T,H,O,N"
+cadena_invertida = inverso_palabra(cadena_original)
+print("Cadena original:", cadena_original)
+print("Cadena invertida:", cadena_invertida)
